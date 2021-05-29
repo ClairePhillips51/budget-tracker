1c7b09cafa4b99f9f72017b1ece2e3cb60a1689a
Claire Phillips <claireacam@gmail.com>
2021-05-29 14:11:11 -0400

    Make service worker handle api fetches correctly.

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,9 +11,6 @@ const FILES_TO_CACHE = [
   '/dist/assets/icons/icon_256x256.png',
   '/dist/assets/icons/icon_384x384.png',
   '/dist/assets/icons/icon_512x512.png',
-//];
-
-//const URLS_TO_CACHE = [
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
@@ -26,15 +23,9 @@ self.addEventListener('install', (event) => {
     caches
       .open(PRECACHE)
       .then((cache) => cache.addAll(FILES_TO_CACHE))
-      // .then(() => {
-      //   for(let url of URLS_TO_CACHE)
-      //   {
-      //     console.log("Cache"+url);
-      //     let request = new Request(url, { mode: 'no-cors' });
-      //     fetch(request).then(response => cache.put(request, response));
-      //   }
-      //   self.skipWaiting()
-      // })
+      .then(() => {
+        self.skipWaiting()
+      })
   );
 });
 
@@ -59,21 +50,46 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/transactions")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
       })
     );
+    return;
   }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME).then((cache) => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
 });
